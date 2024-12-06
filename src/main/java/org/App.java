268eaf1a4efee29b5232cf85fe8a37515e6f4bb0
MOHAK MISHRA <mohakkmishra@MOHAKs-MacBook-Air-m2.local>
2024-12-06 21:50:43 +0530

    Initial commit

---
+++ src/main/java/org/App.java
@@ -0,0 +1,16 @@
+package org;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        String s="sad";
+        System.out.println(s.contains("sa"));
+
+    }
+}

f1d53e16a6111840f09b0dd1ca66410bf311f6ab
Mohak Mishra <mohakmishra2207@gmail.com>
2024-12-26 13:08:21 +0530

    Updated files at An

--- src/main/java/hp/An.java
+++ src/main/java/hp/An.java
@@ -2,6 +2,7 @@ package hp;
 
 public class An {
     public static void main(String[] args) {
-        System.out.println("wo");
+        System.out.println("woah");
+        System.out.println("fixing bugs");
     }
 }

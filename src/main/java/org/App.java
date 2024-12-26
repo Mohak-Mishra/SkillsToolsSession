13812b3e2a3b380b66320fb2a2c57d070808d324
Mohak Mishra <mohakmishra2207@gmail.com>
2024-12-26 13:11:38 +0530

    Updated Sprint1 App.java

--- src/main/java/org/App.java
+++ src/main/java/org/App.java
@@ -10,5 +10,8 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("working in sprint 1");
+        System.out.println("still working on spring 1");
+        System.out.println("now switching to new project of some high priority");
+        System.out.println("completed the work of sprint 1");
     }
 }
